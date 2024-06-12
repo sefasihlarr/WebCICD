90bae9a95afe6a7c58768b8ffde924eb31750534
sefasihlar <sihlarsefa7@gmail.com>
2024-06-12 10:41:20 +0300

    update

--- WebCICD/obj/Debug/net8.0/WebCICD.AssemblyInfo.cs
+++ WebCICD/obj/Debug/net8.0/WebCICD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCICD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0512a8cc799d278d87c3cbbc499cdd8f0d19b5e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCICD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCICD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
